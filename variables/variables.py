4b9362dca74833d9b4aff91765ad44a3372e5856
Akila Madusanka <akilamadusanka471@gmail.com>
2022-09-11 17:45:08 +0530

    variables

---
+++ variables/variables.py
@@ -0,0 +1,9 @@
+firstName="Akila" #string
+lastName="Madusanka" #string
+age=23 #integer
+
+#this line merge two string value
+print("My Name is " + firstName + " " + lastName)
+
+#this line convert int value to string
+print("i am " + str(age) + " years old.")
